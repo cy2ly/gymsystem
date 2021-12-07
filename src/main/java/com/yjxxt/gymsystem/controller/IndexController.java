2bd54d02c88a4a3f2a394b0e7d851ae779f3ca83
DESKTOP-CRU3B54\吴世华 <1327398685@qq.com>
2021-12-07 11:24:52 +0800

    no message

--- src/main/java/com/yjxxt/gymsystem/controller/IndexController.java
+++ src/main/java/com/yjxxt/gymsystem/controller/IndexController.java
@@ -1,6 +1,17 @@
 package com.yjxxt.gymsystem.controller;
 
 import com.yjxxt.gymsystem.base.BaseController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 public class IndexController extends BaseController {
+
+    @RequestMapping({"/","index"})
+    public String index(){
+        return "";
+    }
+
+    @RequestMapping("main")
+    public String main(){
+        return "";
+    }
 }
