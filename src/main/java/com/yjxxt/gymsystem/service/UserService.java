3ed240e320d6f07fe00ec634e4f34dcdb1727d99
DESKTOP-4VAKOBS\lenovo <2978445209@qq.com>
2021-12-07 11:21:17 +0800

    user

---
+++ src/main/java/com/yjxxt/gymsystem/service/UserService.java
@@ -0,0 +1,7 @@
+package com.yjxxt.gymsystem.service;
+
+import com.yjxxt.gymsystem.base.BaseService;
+import com.yjxxt.gymsystem.bean.User;
+
+public class UserService extends BaseService<User,Integer> {
+}
