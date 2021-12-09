5afcaaef5469cbf2e1d8db9657f052f54373cdd8
DESKTOP-CRU3B54\吴世华 <1327398685@qq.com>
2021-12-09 09:59:44 +0800

    课程管理

--- src/main/java/com/yjxxt/gymsystem/mapper/CoachMapper.java
+++ src/main/java/com/yjxxt/gymsystem/mapper/CoachMapper.java
@@ -2,10 +2,9 @@ package com.yjxxt.gymsystem.mapper;
 
 import com.yjxxt.gymsystem.base.BaseMapper;
 import com.yjxxt.gymsystem.bean.Coach;
-import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
 import java.util.Map;
-@Mapper
+
 public interface CoachMapper extends BaseMapper<Coach,Integer> {
 }
